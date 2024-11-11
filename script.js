d4dc39fa4a01a81999b10f5ecde3d214d7c0001c
Anuj-Persnol <anujrdyadav123@gmail.com>
2024-11-11 17:19:40 +0530

    add file

--- script.js
+++ script.js
@@ -15,4 +15,36 @@ nextBtn.addEventListener('click', function () {
         behavior: 'smooth'
     })
 
-})
+}) 
+
+// the video element, button, and icon
+
+const video = document.getElementById('video');
+const playPauseButton = document.getElementById('playPauseButton');
+const playPauseIcon = document.getElementById('playPauseIcon');
+
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play(); 
+    playPauseIcon.classList.remove('fa-play');
+    playPauseIcon.classList.add('fa-pause');
+  } else {
+    video.pause(); 
+    playPauseIcon.classList.remove('fa-pause'); 
+    playPauseIcon.classList.add('fa-play');    
+  }
+}
+
+playPauseButton.addEventListener('click', togglePlayPause);
+
+video.addEventListener('play', function() {
+  playPauseButton.style.display = 'none';
+});
+
+video.addEventListener('pause', function() {
+  playPauseButton.style.display = 'block'; 
+});
+video.addEventListener('ended', function() {
+  playPauseButton.style.display = 'block'; 
+});
+
